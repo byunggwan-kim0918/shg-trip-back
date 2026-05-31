69113466de92f2c1d507a62835185e42a5bd074c
byunggwan-kim0918 <byunggwankim0918@gmail.com>
2026-06-01 01:21:54 +0900

    fix: import 누락 수정

--- src/test/java/com/shg/trip/shgtrip/ShgTripBackApplicationTests.java
+++ src/test/java/com/shg/trip/shgtrip/ShgTripBackApplicationTests.java
@@ -2,6 +2,7 @@ package com.shg.trip.shgtrip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
